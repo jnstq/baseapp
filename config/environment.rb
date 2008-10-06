0f0428f6d6aeadae6704a0e2b443aed9359c9262
Jon Stenqvist <jon.stenqvist@gmail.com>
2008-10-06 23:49:15 +0200

    Added rails edge. Begin with i18n implementation for sv-SE.

--- config/environment.rb
+++ config/environment.rb
@@ -12,6 +12,7 @@ require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
   config.gem 'rspec-rails', :lib => 'spec'
+  
   config.action_controller.session = {
     :session_key => '_base_session',
     :secret      => '7389ea9180b15f1495a5e73a69a893311f859ccff1ffd0fa2d7ea25fdf1fa324f280e6ba06e3e5ba612e71298d8fbe7f15fd7da2929c45a9c87fe226d2f77347'
@@ -22,3 +23,9 @@ Rails::Initializer.run do |config|
   
   config.active_record.observers = :user_observer
 end
+
+I18n.default_locale = 'en-US'
+LOCALES_DIRECTORY = "#{Rails.root}/config/locales/"
+LOCALES_AVAILABLE = Dir["#{LOCALES_DIRECTORY}/*.{rb,yml}"].collect do |locale_file|
+  File.basename(File.basename(locale_file, ".rb"), ".yml")
+end.uniq.sort
