7ff31478a933b0da6135922294e5fd519559dd2c
Jon Stenqvist <jon.stenqvist@gmail.com>
2008-10-07 23:04:58 +0200

    Continue the translation work to sv-SE

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -15,4 +15,26 @@ class ApplicationController < ActionController::Base
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => '9fe6825f97cc334d88925fde5c4808a8'
+  
+  before_filter :set_locale
+  
+  # copy & paste from i18n_demo_app
+  def set_locale
+    # update session if passed
+    session[:locale] = params[:locale] if params[:locale]
+
+    # set locale based on session or default 
+    I18n.locale = session[:locale] || I18n.default_locale
+
+    # load locale from settings
+    @locale_files = []
+    ['yml', 'rb'].each do |type|
+      locale_file = "#{LOCALES_DIRECTORY}#{I18n.locale}.#{type}"
+      if File.exists?(locale_file)
+        @locale_files << locale_file
+        I18n.backend.load_translations locale_file
+      end
+    end
+  end
+  
 end
